c64d560230ce6165494fb728d835283bc6cf6e39
Iasmin-Brito <iasminb@id.uff.br>
2026-04-01 20:31:45 -0300

    Ajustando os comentários

--- analise_imoveis.R
+++ analise_imoveis.R
@@ -7,14 +7,12 @@ head(dados)
 # Estatísticas descritivas
 summary(dados)
 
+# Lendo o arquivo (ajustado para o padrão brasileiro)
 dados <- read.csv2("imoveis.csv")
 dados <- read.csv("imoveis.csv", sep = ";", dec = ",")
 head(dados)
 
-# 1. Lendo o arquivo (ajustado para o padrão brasileiro)
-dados <- read.csv("imoveis.csv", sep = ";", dec = ",")
-
-# 2. Limpeza pesada das colunas (remove R$, espaços e pontos)
+# Limpando pesado as colunas (remove R$, espaços e pontos)
 limpar_numero <- function(x) {
   as.numeric(gsub("[^0-9,]", "", as.character(x)))
 }
@@ -23,13 +21,13 @@ dados$preco <- limpar_numero(dados$preco)
 dados$metragem <- as.numeric(as.character(dados$metragem))
 dados$imposto <- as.numeric(as.character(dados$imposto))
 
-# 3. Gerando os gráficos
+# Gerando os gráficos
 # Histograma
 hist(na.omit(dados$preco), 
      main="Histograma do Preço", 
      col="lightblue", xlab="Preço")
 
-# Dispersão (O que estava faltando!)
+# Dispersão 
 plot(dados$metragem, dados$imposto, 
      main="Imposto vs Metragem", 
      xlab="Metragem (x)", ylab="Imposto (y)", 
